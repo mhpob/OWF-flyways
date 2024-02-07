2a4868cd29572390bf284ade15fb93169b0bb7e1
Mike O'Brien <mikeob9@gmail.com>
2024-02-07 15:11:28 -0500

    try geom_textsf

--- code/north_sea.R
+++ code/north_sea.R
@@ -1,11 +1,12 @@
 library(sf)
 library(ggplot2)
+library(geomtextpath)
 
-herring <- read_sf('herring_corten.gpkg')
-mackerel <- read_sf('mackerel_daan.gpkg')
-whiting <- read_sf('whiting_gonzalez-irusta.gpkg')
+herring <- read_sf('data/spatial/herring_corten.gpkg')
+mackerel <- read_sf('data/spatial/mackerel_daan.gpkg')
+whiting <- read_sf('data/spatial/whiting_gonzalez-irusta.gpkg')
 
-wind_europe <- read.csv('OWF Names_data.csv') |> 
+wind_europe <- read.csv('data/spatial/OWF Names_data.csv') |> 
   st_as_sf(coords = c('GIS.Longitude', 'GIS.Latitude'),
            crs = 4326)
 
@@ -19,22 +20,13 @@ ggplot() +
   geom_sf(data = wind_europe,
           aes(fill = Status,
               size = Total.Project.Capacity), pch = 21) +
-geom_sf(data = plot_dat, aes(color = species, lty = season)) +
+  scale_size(guide = 'none') +
+  geom_sf(data = dplyr::bind_rows(herring, mackerel),
+              aes(color = species, lty = loop)) +
+  geom_textsf(data = dplyr::bind_rows(herring, mackerel),
+          aes(color = species, label = season),
+          linecolor = NA, vjust = -0.1, size = 3) +
+  geom_sf(data = whiting, aes(color = species)) +
   scale_linetype(na.value = 'solid') +
   coord_sf(xlim = c(-5, 10), ylim = c(49, 62.5)) +
   theme_minimal()
-
-plot(x = c(-5, 10),type = 'n', y = c(49, 62.5), asp=1)
-plot(st_geometry(land),
-     add = T)
-north_sea <- land |> 
-  st_transform(32631) 
-
-ggplot(north_sea) +
-  geom_sf() +
-  coord_sf(xlim = c(-2e5, 9e5), ylim = c(5.45e6, 7e6))
-
-?north_sea |> 
-  st_crop(c(xmin = -2e5, xmax = 9e5, ymin = 5.45e6, ymax= 8.2e6)) |> 
-  st_geometry() |> 
-  plot()
