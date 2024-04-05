fe434675b950081dc5ae3e150ed8c6cc5ac70091
Mike O'Brien <mikeob9@gmail.com>
2024-04-05 11:36:40 -0400

    change scale to m2

--- code/scale_figure.R
+++ code/scale_figure.R
@@ -6,17 +6,20 @@ scale$label <- gsub(' n ', '\n', scale$label)
 
 
 ggplot(data = scale) +
-  geom_rect(aes(xmin = space_min, xmax = space_max,
+  geom_rect(aes(xmin = space_min/(1000^2), xmax = space_max/(1000^2),
                 ymin = time_min, ymax = time_max, color = label),
             fill = NA, linewidth = 2,
             show.legend = F) +
-  geom_label(aes(x = space_min,y = time_min, label = label, color = label),
+  geom_label(aes(x = space_min/(1000^2),y = time_min, label = label, color = label),
              show.legend = F, hjust = 0, vjust = 0) +
   scale_y_log10(breaks = c(60, 60*60, 60*60*24, 60*60*24*7, 60*60*24*7*4.5,
                            60*60*24*7*52, 60*60*24*7*52*10,
                            60*60*24*7*52*10*10),
                 labels = c('min', 'hour', 'day', 'week', 'month', 'year',
                            'decade', 'century')) +
-  scale_x_log10(breaks = 10 ^ seq(2, 18, 2)) +
-  labs(x = bquote(mm^2), y = NULL) +
-  theme_minimal()
+  scale_x_log10(breaks = 10 ^ seq(1, 13, 2)) +
+  labs(x = bquote(m^2), y = NULL) +
+  scale_color_brewer(type = 'qual',
+                     palette = 'Set1')+
+  theme_minimal() +
+  theme(panel.grid.minor = element_blank())
