ef0505168635b5087a63efab7d30b9b17dd9fde9
Mike O'Brien <mikeob9@gmail.com>
2024-05-14 17:12:16 -0400

    add legends and colors

--- code/china_turtles.R
+++ code/china_turtles.R
@@ -2,7 +2,7 @@ library(sf)
 library(ggplot2)
 library(dplyr)
 
-land <- 'c:/users/darpa2/analysis/rtwb-flyway/data/spatial/coastline.gpkg' |> 
+land <- '/vsizip/c:/users/darpa2/downloads/ne_10m_land.zip/ne_10m_land.shp' |> 
   read_sf()
 
 ch_wind_capacity <- read.csv('data/ch_wind_capacity.csv')
@@ -13,7 +13,8 @@ provinces <- '/vsizip/data/spatial/chn_adm_ocha_2020_shp.zip/chn_admbnda_adm1_oc
   right_join(ch_wind_capacity)
 
 turtle_sites <- st_read('data/spatial/turtle_flyway.gpkg',
-                        layer = 'turtle_flyway')
+                        layer = 'turtle_flyway') |> 
+  mutate(species = ifelse(species == 'hawkbill', 'hawksbill',species))
 turtle_paths <- st_read('data/spatial/turtle_loops.gpkg',
                         layer = 'migration_loops')
 turtle_paths[c(1, 5), 'geom'] <- st_reverse(turtle_paths[c(5, 1), 'geom'])
@@ -27,13 +28,23 @@ ggplot() +
           aes(fill = Capacity)) +
   geom_sf_label(data = provinces,
           aes(label = Province),
-          nudge_x = -2,
-          nudge_y = -0.5) +
+          nudge_x = -2, nudge_y = c(0,0,-1,0,0,0,0,0,0)) +
   geom_sf(data = turtle_sites,
-          aes(color = species, shape = type)) +
+          aes(color = species, shape = type), size = 4) +
+  scale_color_brewer(type = 'qual', palette = 'Set1',
+                     guide =  guide_legend(order = 1)) +
   geom_sf(data = turtle_paths, arrow = arrow(length = unit(0.1, "inches")),
-          aes(color = type)) +
-  scale_fill_viridis_c(option = 'B', trans = 'log') +
+          aes(linetype = type)) +
+  scale_fill_viridis_c(option = 'B', trans = 'log', breaks = c(50, 500, 3000)) +
   coord_sf(xlim = c(107, 135), ylim = c(15, 35)) +
-  labs(x = NULL, y = NULL) +
-  theme_minimal()
+  labs(x = NULL, y = NULL, color = 'Species', linetype = NULL, shape = NULL,
+       fill = 'Capacity (MW)') +
+  theme_minimal()+
+  theme(legend.key.width = unit(2, 'line'),
+        legend.position =  c(0.9, 0.33),
+        legend.text = element_text(size =12),
+        legend.title = element_text(size = 14),
+        axis.text = element_text(size = 12))+
+  guides(linetype = guide_legend(order = 2,
+                                 override.aes = list(alpha = 0.7)),
+         shape = guide_legend(order  = 2))
