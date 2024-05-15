7d5ef25c31382bae91c7db67668c802f675d3adb
Mike O'Brien <mikeob9@gmail.com>
2024-05-15 15:36:38 -0400

    update in prep for submittal

--- code/north_sea.R
+++ code/north_sea.R
@@ -1,32 +1,64 @@
 library(sf)
+library(dplyr)
 library(ggplot2)
 library(geomtextpath)
 
 herring <- read_sf('data/spatial/herring_corten.gpkg')
-mackerel <- read_sf('data/spatial/mackerel_daan.gpkg')
+mackerel <- read_sf('data/spatial/mackerel_daan.gpkg') |> 
+  mutate(loop = ifelse(loop == 'west', 'south', loop),
+         species = 'Atl. mackerel')
 whiting <- read_sf('data/spatial/whiting_gonzalez-irusta.gpkg')
 
 wind_europe <- read.csv('data/spatial/OWF Names_data.csv') |> 
   st_as_sf(coords = c('GIS.Longitude', 'GIS.Latitude'),
            crs = 4326)
 
-land <- '/vsizip/vsicurl/https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_land.zip' |> 
+land <- '/vsizip/data/spatial/ne_10m_land.zip/ne_10m_land.shp' |> 
   read_sf()
 
 plot_dat <- dplyr::bind_rows(herring, mackerel, whiting)
 
-ggplot() +
+the_plot <-
+  ggplot() +
   geom_sf(data = land) +
   geom_sf(data = wind_europe,
           aes(fill = Status,
               size = Total.Project.Capacity), pch = 21) +
-  scale_size(guide = 'none') +
+  scale_size(guide =  guide_legend(order = 2)) +
+  geom_sf(data = whiting, aes(color = species),
+          linewidth = 1) +
   geom_sf(data = dplyr::bind_rows(herring, mackerel),
-              aes(color = species, lty = loop)) +
+              aes(color = species, lty = loop),
+          linewidth = 1) +
   geom_textsf(data = dplyr::bind_rows(herring, mackerel),
           aes(color = species, label = season),
-          linecolor = NA, vjust = -0.1, size = 3) +
-  geom_sf(data = whiting, aes(color = species)) +
-  scale_linetype(na.value = 'solid') +
+          vjust = -0.1, linecolor = NA) +
+  scale_fill_brewer(palette = 'YlOrBr',
+                    guide = guide_legend(order = 3))+
+  scale_color_brewer(palette = 'Accent',
+                     guide = guide_legend(order = 1)) +
+  scale_linetype(guide = 'none', na.value = 'solid') +
+  labs(color = 'Species', size = 'OW Capacity (MW)',
+       fill = 'OW Status') +
   coord_sf(xlim = c(-5, 10), ylim = c(49, 62.5)) +
-  theme_minimal()
+  theme_minimal() +
+    theme(
+      legend.title = element_text(size = 12),
+      legend.text = element_text(size = 11)
+    ) +
+  guides(fill = guide_legend(override.aes = list(size = 5)))
+
+library(ragg)
+
+##630w*789h  0.66
+agg_png('fig.png',
+        width = 800,
+        height = 800/.95,
+        scaling = 1.5)
+
+the_plot
+
+dev.off()
+
+
+ggsave('file.eps', device = 'eps')
