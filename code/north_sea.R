51747b621807dface006735107b9d769e7bf6bd3
Mike O'Brien <mikeob9@gmail.com>
2024-05-22 14:03:14 -0400

    update NS fig for data and remove owf status coloring

--- code/north_sea.R
+++ code/north_sea.R
@@ -10,7 +10,7 @@ mackerel <- read_sf('data/spatial/mackerel_daan.gpkg') |>
 whiting <- read_sf('data/spatial/whiting_gonzalez-irusta.gpkg')
 
 wind_europe <- read.csv('data/spatial/OWF Names_data.csv') |> 
-  st_as_sf(coords = c('GIS.Longitude', 'GIS.Latitude'),
+  st_as_sf(coords = c('Avg..Gis.Longitude', 'Gis.Latitude'),
            crs = 4326)
 
 land <- '/vsizip/data/spatial/ne_10m_land.zip/ne_10m_land.shp' |> 
@@ -22,9 +22,8 @@ the_plot <-
   ggplot() +
   geom_sf(data = land) +
   geom_sf(data = wind_europe,
-          aes(fill = Status,
-              size = Total.Project.Capacity), pch = 21) +
-  scale_size(guide =  guide_legend(order = 2)) +
+          aes(size = Total.Project.Capacity),
+          pch = 21, alpha = 0.7) +
   geom_sf(data = whiting, aes(color = species),
           linewidth = 1) +
   geom_sf(data = dplyr::bind_rows(herring, mackerel),
@@ -33,11 +32,11 @@ the_plot <-
   geom_textsf(data = dplyr::bind_rows(herring, mackerel),
           aes(color = species, label = season),
           vjust = -0.1, linecolor = NA) +
-  scale_fill_brewer(palette = 'YlOrBr',
-                    guide = guide_legend(order = 3))+
   scale_color_brewer(palette = 'Accent',
                      guide = guide_legend(order = 1)) +
   scale_linetype(guide = 'none', na.value = 'solid') +
+  scale_size(breaks = c(100, 500, 1000),
+             guide = guide_legend(order = 2)) +
   labs(color = 'Species', size = 'OW Capacity (MW)',
        fill = 'OW Status') +
   coord_sf(xlim = c(-5, 10), ylim = c(49, 62.5)) +
@@ -51,7 +50,7 @@ the_plot <-
 library(ragg)
 
 ##630w*789h  0.66
-agg_png('fig.png',
+agg_png('fig4.png',
         width = 800,
         height = 800/.95,
         scaling = 1.5)
@@ -61,4 +60,4 @@ the_plot
 dev.off()
 
 
-ggsave('file.eps', device = 'eps')
+# ggsave('file.eps', device = 'eps')
