9b04a7d993b2f59593ac4e6dd5229ae54c56ac5e
Mike O'Brien <mikeob9@gmail.com>
2024-07-25 13:29:04 -0400

    adjust f1 for ms revisions

--- code/scale_figure.R
+++ code/scale_figure.R
@@ -4,14 +4,27 @@ library(ggplot2)
 scale <- fread('data/scale_figure_data.csv')
 scale$label <- gsub(' n ', '\n', scale$label)
 
+label_loc <- copy(scale)
+label_loc[grepl('Pop', label), 'space_min'] <- 
+  label_loc[grepl('Pop', label), 'space_max']
+label_loc[grepl('EMF', label), 'time_min'] <- 
+  label_loc[grepl('EMF', label), 'time_max']
 
-ggplot(data = scale) +
-  geom_rect(aes(xmin = space_min/(1000^2), xmax = space_max/(1000^2),
-                ymin = time_min, ymax = time_max, color = label),
+
+scale_fig <-
+  ggplot() +
+  geom_rect(data = scale,
+            aes(xmin = space_min/(1000^2), xmax = space_max/(1000^2),
+                ymin = time_min, ymax = time_max, color = label,
+                linetype = type),
             fill = NA, linewidth = 2,
             show.legend = F) +
-  geom_label(aes(x = space_min/(1000^2),y = time_min, label = label, color = label),
-             show.legend = F, hjust = 0, vjust = 0) +
+  geom_label(data = label_loc,
+             aes(x = space_min/(1000^2), y = time_min,
+                 label = label, color = label),
+             show.legend = F,
+             hjust = c(rep(0, 2), 1, rep(0, 4)),
+             vjust = c(rep(0, 5), 1, 0)) +
   scale_y_log10(breaks = c(60, 60*60, 60*60*24, 60*60*24*7, 60*60*24*7*4.5,
                            60*60*24*7*52, 60*60*24*7*52*10,
                            60*60*24*7*52*10*10),
@@ -20,6 +33,20 @@ ggplot(data = scale) +
   scale_x_log10(breaks = 10 ^ seq(1, 13, 2)) +
   labs(x = bquote(m^2), y = NULL) +
   scale_color_brewer(type = 'qual',
-                     palette = 'Set1')+
+                     palette = 'Dark2') +
+  scale_linetype_manual(values = c('dashed', 'solid')) +
   theme_minimal() +
-  theme(panel.grid.minor = element_blank())
+  theme(panel.grid.minor = element_blank(),
+        plot.margin = unit(c(0, 0, 0, 0), 'mm'))
+
+library(ragg)
+agg_png("figures/figure1.png",
+        width = 170,
+        height = 180,
+        units = 'mm',
+        res = 500,
+        scaling = 1.3)
+
+scale_fig
+
+dev.off()
